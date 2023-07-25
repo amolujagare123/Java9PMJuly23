f1fea6a38c617d697c023cc56a2f69a1d19c4040
amolu <amolujagare@gmail.com>
2023-07-25 22:24:07 +0530

    Initial commit

---
+++ src/DataTypeDemo.java
@@ -0,0 +1,25 @@
+public class DataTypeDemo {
+
+    public static void main(String[] args) {
+// integer
+        byte b = -128;
+        short s = -32768;
+        int n = 1787877787;
+        long l = 5345678914343431234l;
+
+// decimal
+        float f = 1.3f;
+        double d = 10.56;
+
+        // boolean
+        boolean b1 = true;
+        boolean b2 = false;
+
+        // character
+
+        char c1  ='j';
+        char c2  ='4';
+        char c3  ='$';
+    }
+
+}
---
+++ src/DataTypeDemo2.java
@@ -0,0 +1,14 @@
+public class DataTypeDemo2 {
+
+    public static void main(String[] args) {
+
+        char c = 78 ;
+
+        int d = 'i';
+
+        System.out.println("d="+d);
+        System.out.println("c="+c);
+
+    }
+
+}
